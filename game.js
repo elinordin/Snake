8210ebaac905f8010965708b8c7719bf0ddd13f9
elinordin <elin.evelina@live.se>
2020-10-09 21:50:28 +0200

    Fix with highscore-bug, almost done

--- game.js
+++ game.js
@@ -2,13 +2,13 @@
 let canvas = document.getElementById("gameboard");
 let ctx = canvas.getContext("2d");
 let gameInterval;
-let h1name = "Player 1";
+/*let h1name = "Player 1";
 let h1score = 20;
 let h2name = "Player 2";
 let h2score = 10;
 let h3name = "Player 3";
-let h3score = 0;
-/*let highscore = [{ //Creates array with names and score
+let h3score = 0;*/
+let highscore = [{ //Creates array with names and score
   name: "Player1",
   score: 20
 }, {
@@ -17,8 +17,9 @@ let h3score = 0;
 }, {
   name: "Player3",
   score: 0
-}]*/
-let input = document.getElementById("name-input");
+}]
+let nameInput = document.getElementById("name-input");
+let playerName;
 let score = 0;
 
 const foodSize = 20;
@@ -40,7 +41,7 @@ let snakeTail = [{
 
 let colliding = false;
 let distance = 20;
-let speed = 400; //400 start-speed
+let speed = 200; //400 start-speed
 let rightInterval, downInterval, leftInterval, upInterval;
 let rightCycle = false; //Detects wether the button has already been pressed
 let downCycle = false;
@@ -63,67 +64,35 @@ function draw() {
   checkCollision();
 
   if (colliding) { //If snake's position is equal to the gameboard or tail
+    console.log("game over");
     clearInterval(gameInterval); //Stop drawing
-    if (score > h1score){ //If score is higher than leading score
-      document.getElementById("new-highscore").style.display = "block"; //Display highscore window
-      input.addEventListener("input", function(e){ //Listen for the user to type, the e in this case is what the user inputs
-        h3name = h2name;
-        h3score = h2score;
-        h2name = h1name;
-        h2score = h1score;
-        h1name = e.target.value;
-        h1score = score;
-        //highscore.splice(0, 0, (new Highscore(e.target.value, score)));
-        document.addEventListener("keydown", function(e){ //If enter-key is pressed
-          if (e.key == "Enter"){
-            highscoreToGameOver();
-          }
-        })
-      })
-    } else if (score > h2score){ //If score is higher than second score
-      document.getElementById("new-highscore").style.display = "block";
-      input.addEventListener("input", function(e){
-        h3name = h2name;
-        h3score = h2score;
-        h2name = e.target.value;
-        h2score = score;
-        //highscore.splice(1, 0, (new Highscore(e.target.value, score)));
-        document.addEventListener("keydown", function(e){
-          if (e.key == "Enter"){
-            highscoreToGameOver();
-          }
-        })
-      })
-    } else if (score > h3score){ //If score is higher than third score THIS ONE WORKS
-      document.getElementById("new-highscore").style.display = "block";
-      input.addEventListener("input", function(e){ //Listen for the user to type, the e in this case is what the user inputs
-        h3name = e.target.value;
-        h3score = score;
-        //highscore.splice(2, 0, (new Highscore(e.target.value, score))); //Position of new array item, how many are being removed, values to send to constructor function
-        document.addEventListener("keydown", function(e){
-          if (e.key == "Enter"){
-            highscoreToGameOver();
-          }
-        })
-      })
-    } else {
+    if (score <= highscore[2].score) { //If score did not make it to top 3
+      displayGameOver();
+    } else { //If score made it to the scoreboard
       displayHighscore();
-      document.getElementById("game-over").style.display = "block"; //Show game-over window
+      document.addEventListener("keydown", function(e) { //If enter-key is pressed
+        if (e.key == "Enter") {
+          playerName = nameInput.value; //Captures users input
+          setHighScore();
+          displayScoreboard();
+          highscoreToGameOver();
+        }
+      })
     }
-  };
+  }
 
   if (snakeHeadFromLeft == foodFromLeft && snakeHeadFromTop == foodFromTop) { //If the snakes position is equal to the foods position
     newPlaceForFood();
     score += 10; //Add 10 points to score
     increaseSpeed(score);
     snakePath.push(new Path(snakeHeadFromLeft, snakeHeadFromTop)); //Add one item to Path-array
-    if (rightCycle){ //This if/else-statement is to make sure that the head moves one step before adding the tail to its position (or else they collide)
+    if (rightCycle) { //This if/else-statement is to make sure that the head moves one step before adding the tail to its position (or else they collide)
       snakeHeadFromLeft += distance;
-    } else if (downCycle){
+    } else if (downCycle) {
       snakeHeadFromTop += distance;
-    } else if (leftCycle){
+    } else if (leftCycle) {
       snakeHeadFromLeft -= distance;
-    } else if (upCycle){
+    } else if (upCycle) {
       snakeHeadFromTop -= distance;
     }
     addTail(i);
@@ -133,7 +102,7 @@ function draw() {
 
 
 //------------------------FUNCTIONS------------------------
-function setupEventListeners(){
+function setupEventListeners() {
   document.addEventListener("keydown", keyDownHandler);
   document.getElementById("restart").addEventListener("click", reset); //If restart-button is clicked - reset game
   document.getElementById("no-restart").addEventListener("click", displayLearnings);
@@ -148,7 +117,7 @@ function drawFood() {
   ctx.closePath();
 }
 
-function newPlaceForFood(){
+function newPlaceForFood() {
   foodFromLeft = randomLeftNumber(); //Randomizes new spot from left
   foodFromTop = randomTopNumber(); //Randomizes new spot from top
 }
@@ -174,7 +143,7 @@ function drawSnakeHead() {
   ctx.closePath();
 }
 
-function Path(headFromLeft, headFromTop){ //Constructor function creating new path-parts
+function Path(headFromLeft, headFromTop) { //Constructor function creating new path-parts
   this.headFromLeft = headFromLeft;
   this.headFromTop = headFromTop;
 }
@@ -253,7 +222,7 @@ function moveRight() {
   snakePath.push(new Path(snakeHeadFromLeft, snakeHeadFromTop)); //Add one to the path-array with current position of head
   snakeHeadFromLeft += distance; //Move head from the left
   applyPathToTail(); //Add the value of snakePath[i] to snakeTail[i], this one is being used in the collision detection, if I apply it to the drawTail() it's making everything 20 px after the head, the drawTail function now has the direct velues of snakePath[i]
-  if (snakePath.length > snakeTail.length){
+  if (snakePath.length > snakeTail.length) {
     snakePath.shift(); // Remove the first item in path-array
   }
 }
@@ -263,7 +232,7 @@ function moveDown() {
   snakePath.push(new Path(snakeHeadFromLeft, snakeHeadFromTop));
   snakeHeadFromTop += distance; //Move head 20px from the top
   applyPathToTail();
-  if (snakePath.length > snakeTail.length){
+  if (snakePath.length > snakeTail.length) {
     snakePath.shift();
   }
 }
@@ -273,7 +242,7 @@ function moveLeft() {
   snakePath.push(new Path(snakeHeadFromLeft, snakeHeadFromTop));
   snakeHeadFromLeft -= distance; //Move head 20px closer to the left
   applyPathToTail();
-  if (snakePath.length > snakeTail.length){
+  if (snakePath.length > snakeTail.length) {
     snakePath.shift();
   }
 }
@@ -283,56 +252,91 @@ function moveUp() {
   snakePath.push(new Path(snakeHeadFromLeft, snakeHeadFromTop)); //Push in the current values of head into the path-array
   snakeHeadFromTop -= distance; //Move head 20px closer to the top
   applyPathToTail();
-  if (snakePath.length > snakeTail.length){
+  if (snakePath.length > snakeTail.length) {
     snakePath.shift();
   }
 }
 
-function applyPathToTail(){  //Add the value of snakePath[i] to snakeTail[i], this one is being used in the collision detection, if I apply it to the drawTail() it's making everything 20 px after the head, the drawTail function now has the direct velues of snakePath[i]
-  for (var i = 0; i < snakeTail.length; i++){
+function applyPathToTail() { //Add the value of snakePath[i] to snakeTail[i], this one is being used in the collision detection, if I apply it to the drawTail() it's making everything 20 px after the head, the drawTail function now has the direct velues of snakePath[i]
+  for (var i = 0; i < snakeTail.length; i++) {
     snakeTail[i].tailFromLeft = snakePath[i].headFromLeft;
     snakeTail[i].tailFromTop = snakePath[i].headFromTop;
   }
 }
 
-function checkCollision(){
-  for (var i = 0; i < snakeTail.length; i++){
-    if (snakeHeadFromLeft == snakeTail[i].tailFromLeft && snakeHeadFromTop == snakeTail[i].tailFromTop){
+function checkCollision() {
+  for (var i = 0; i < snakeTail.length; i++) {
+    if (snakeHeadFromLeft == snakeTail[i].tailFromLeft && snakeHeadFromTop == snakeTail[i].tailFromTop) {
       colliding = true;
     }
   }
-  if (snakeHeadFromLeft == -20 || snakeHeadFromLeft == canvas.width || snakeHeadFromTop == -20 || snakeHeadFromTop == canvas.height){
+  if (snakeHeadFromLeft == -20 || snakeHeadFromLeft == canvas.width || snakeHeadFromTop == -20 || snakeHeadFromTop == canvas.height) {
     colliding = true;
   }
 }
 
-function displayHighscore(){
-      document.getElementById("win-name").innerHTML = h1name;
-      document.getElementById("win-score").innerHTML = h1score;
-      document.getElementById("second-name").innerHTML = h2name;
-      document.getElementById("second-score").innerHTML = h2score;
-      document.getElementById("third-name").innerHTML = h3name;
-      document.getElementById("third-score").innerHTML = h3score;
-}
-
-/*function Highscore(e, score){
-  this.name = e;
-  this.score = score;
-}*/
-
-
-function highscoreToGameOver(){
-  document.getElementById("new-highscore").style.display = "none"; //Hide new-highscore
-  displayHighscore();
+function displayGameOver() {
+  console.log("You did not make it to the scoreboard");
+  displayScoreboard();
   document.getElementById("game-over").style.display = "block"; //Show game-over window
 }
 
-function reset(){
+function displayHighscore() {
+  document.getElementById("new-highscore").style.display = "block"; //Show highscore window
+}
+
+//First time it loops through one alternative, second round two alternatives and third round three alternatives - WHY?
+function setHighScore() {
+  if (score > highscore[2].score && score <= highscore[1].score){ //If score is higher than current third place
+    console.log("third place");
+    highscore.splice(2, 0, (new Highscore(playerName, score)))
+    highscore.pop()
+
+  } else if (score > highscore[1].score && score <= highscore[0].score){
+    console.log("second place");
+    highscore.splice(1, 0, (new Highscore(playerName, score)))
+    highscore.pop()
+
+  } else if (score > highscore[0].score) { //If score is higher than current first place
+    console.log("first place");
+    highscore.splice(0, 0, (new Highscore(playerName, score)))
+    highscore.pop()
+
+  }
+}
+
+function Highscore(e, score) {
+  this.name = e;
+  this.score = score;
+}
+
+function displayScoreboard() {
+  document.getElementById("win-name").innerHTML = highscore[0].name;
+  document.getElementById("win-score").innerHTML = highscore[0].score;
+  document.getElementById("second-name").innerHTML = highscore[1].name;
+  document.getElementById("second-score").innerHTML = highscore[1].score;
+  document.getElementById("third-name").innerHTML = highscore[2].name;
+  document.getElementById("third-score").innerHTML = highscore[2].score;
+  /*document.getElementById("win-name").innerHTML = h1name;
+  document.getElementById("win-score").innerHTML = h1score;
+  document.getElementById("second-name").innerHTML = h2name;
+  document.getElementById("second-score").innerHTML = h2score;
+  document.getElementById("third-name").innerHTML = h3name;
+  document.getElementById("third-score").innerHTML = h3score;*/
+}
+
+
+function highscoreToGameOver() {
+  document.getElementById("new-highscore").style.display = "none"; //Hide new-highscore
+  document.getElementById("game-over").style.display = "block"; //Show game-over window
+}
+
+function reset() {
   colliding = false;
   document.getElementById("game-over").style.display = "none";
   gameInterval = setInterval(draw, 20);
   score = 0;
-  speed = 400;
+  speed = 200; //400
   snakeHeadFromLeft = 20;
   snakeHeadFromTop = 0;
   snakePath = [{
@@ -354,7 +358,7 @@ function reset(){
   document.getElementById("score").innerHTML = score;
 }
 
-function displayLearnings(){
+function displayLearnings() {
   document.getElementById("game-over").style.display = "none"; //Do not display game-over screen
   document.getElementById("crawl").style.animation = "crawl 50s linear"; //Apply the crawl animation
   document.getElementById("learnings").style.display = "flex"; //Display my learnings
